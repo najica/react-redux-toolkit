b109e8bd691cf94ca69c2f27449e29a2cc687871
najica <najicans@gmail.com>
2024-03-06 14:38:18 +0100

    add buttons add amount and reset all

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
 
 export const Counter = () => {
   const count = useSelector((state: any) => state.counter.count);
   const dispatch = useDispatch();
 
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e: any) => setIncrementAmount(e.target.value)}
+      ></input>
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset All</button>
       </div>
     </section>
   );
